55b79f8c20749849a7cac2d75a8ff79fad62f7f4
Jason Barto <jason.p.barto@gmail.com>
2025-10-05 02:05:38 +0100

    added tofu install to build script

--- cicd/build.sh
+++ cicd/build.sh
@@ -1,4 +1,17 @@
-#!/bin/sh
+#!/bin/bash
+
+# install opentofu
+# Download the installer script:
+curl --proto '=https' --tlsv1.2 -fsSL https://get.opentofu.org/install-opentofu.sh -o install-opentofu.sh
+
+# Give it execution permissions:
+chmod +x install-opentofu.sh
+
+# Run the installer:
+./install-opentofu.sh --install-method deb
+
+# Remove the installer:
+rm -f install-opentofu.sh
 
 pushd terraform
 tofu fmt
