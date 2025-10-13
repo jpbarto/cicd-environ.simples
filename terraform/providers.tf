6871eb5c0c4d053f518c6551567c00b916384d16
Jason Barto <jason.p.barto@gmail.com>
2025-10-13 16:08:35 +0100

    added outputs qualifier to remoate state reference

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,22 +2,13 @@ provider "aws" {
   region = var.aws_region
 }
 
-data "terraform_remote_state" "k8s_cluster" {
-  backend = "s3"
-  config = {
-    bucket = "cicd-environ-tofu-state-prod-ab12cf"
-    key = "prod/platform/terraform.tfstate"
-    region = "us-east-2"
-  }
-}
-
 provider "helm" {
   kubernetes = {
-    host                   = data.terraform_remote_state.eks_cluster_endpoint
-    cluster_ca_certificate = base64decode(data.terraform_remote_state.eks_cluster_ca_certificate)
+    host                   = data.terraform_remote_state.platform.outputs.eks_cluster_endpoint
+    cluster_ca_certificate = base64decode(data.terraform_remote_state.platform.outputs.eks_cluster_ca_certificate)
     exec = {
       api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["eks", "get-token", "--cluster-name", data.terraform_remote_state.eks_cluster_name]
+      args        = ["eks", "get-token", "--cluster-name", data.terraform_remote_state.platform.outputs.eks_cluster_name]
       command     = "aws"
     }
   }
