f05065fdad553aeb2be410b25c2ee093598ab84a
Jason Barto <jason.p.barto@gmail.com>
2025-10-17 11:35:42 +0100

    renamed state bucket

--- terraform/data.tf
+++ terraform/data.tf
@@ -1,7 +1,7 @@
 data "terraform_remote_state" "platform" {
   backend = "s3"
   config = {
-    bucket = "cicd-environ-tofu-state-prod-ab12cf"
+    bucket = "cicd-environ-tofu-state-prod-ab12cr"
     key = "prod/platform/terraform.tfstate"
     region = "us-east-2"
   }
